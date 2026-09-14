66e261ead9d784cab86551b6e9dfa4caae76212d
Abhishek <raoabhishek0510@gmail.com>
2026-09-14 18:43:30 +0530

    Commit14

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.RITUMATI_CONFIG={
   spreadsheetId:'1Vn8iCxg6qVE7cKFi3DK0CvZR2G6A-ySbpqyAhNqMACg',
-  joinFormUrl:'https://script.google.com/macros/s/AKfycbyeO14zl-YM-YWyPMSqGxqMMWuSCgVq-_tTn_Ak8VWngYcIuxc41Qw-UZ0XApc14WGm/exec'
+  joinFormUrl:'https://script.google.com/macros/s/AKfycbwjZtwV7nsFBp9OYNdk0g2Er-GcZf7LL5eU6SiEefUHf2xbVckqapCMeHfDuxykxBbZ/exec'
 };
